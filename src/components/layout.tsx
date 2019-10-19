eb58b82bd57e434beecd9cafdaee0d9360b438c5
robertma <robertma@easyops.cn>
2019-10-19 13:05:29 +0800

    feat(comment): click heart request animation

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import styled  from 'styled-components'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import "./layout.css"
 import styles from "./head.module.css"
 
@@ -8,7 +8,8 @@ import { useStaticQuery, graphql } from "gatsby";
 import { AsideComponent } from "./off-canvas-sidebar/aside"
 import { ScrollButton } from "./scoll-button/ScrollButton";
 import { Footer } from "./footer/Footer";
-import {ToggleWrapper} from "./toggle/ToggleWrapper";
+import { ToggleWrapper } from "./toggle/ToggleWrapper";
+import { css, randomColor } from "./utils";
 
 export const Layout = (props: any) => {
   const data = useStaticQuery(graphql`
@@ -26,7 +27,61 @@ export const Layout = (props: any) => {
     color: ${({ theme }) => theme.color};
     background-color: ${({ theme }) => theme.bg};
     min-height: 100vh;
-    `
+    `;
+
+  const hearts = [];
+  const [heart, setHeart] = useState([]);
+  const requestRef = React.useRef(0);
+
+  const createHeart = (event) => {
+    if (!event) {
+      return;
+    }
+    const d = document.createElement("div");
+    d.className = "heart";
+    heart.push({
+      el: d,
+      x: event.clientX - 5,
+      y: event.clientY - 5,
+      scale: 1,
+      alpha: 1,
+      color: randomColor()
+    });
+    document.body.appendChild(d);
+  }
+
+  const attachEvent = () => {
+    const old = typeof window.onclick === "function" && window.onclick;
+    window.onclick = function (event) {
+      old && old();
+      createHeart(event);
+    }
+  }
+
+  const gameloop = () => {
+    for (let i = 0; i < hearts.length; i++) {
+      if (hearts[i].alpha <= 0) {
+        document.body.removeChild(hearts[i].el);
+        hearts.splice(i, 1);
+        continue;
+      }
+      hearts[i].y--;
+      hearts[i].scale += 0.004;
+      hearts[i].alpha -= 0.013;
+      hearts[i].el.style.cssText = "left:" + hearts[i].x + "px;top:" + hearts[i].y + "px;opacity:" + hearts[i].alpha + ";transform:scale(" + hearts[i].scale + "," + hearts[i].scale + ") rotate(45deg);background:" + hearts[i].color;
+    }
+    setHeart(hearts);
+    requestAnimationFrame(gameloop);
+  }
+
+  css(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}");
+  attachEvent();
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(gameloop);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, []); // Make sure the effect runs only once
+
 
   return (
     <>
---
+++ src/components/utils.tsx
@@ -0,0 +1,14 @@
+export function css(css){
+  var style = document.createElement("style");
+  style.type="text/css";
+  try{
+      style.appendChild(document.createTextNode(css));
+  }catch(ex){
+      style.styleSheet.cssText = css;
+  }
+  document.getElementsByTagName('head')[0].appendChild(style);
+}
+
+export function randomColor(){
+  return "rgb("+(~~(Math.random()*255))+","+(~~(Math.random()*255))+","+(~~(Math.random()*255))+")";
+}
